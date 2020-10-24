bef9b2c015fec4983557a08bfd78647978b8668f
Yunpeng Niu <neilniuyunpeng@gmail.com>
2020-10-24 17:15:22 +0800

    Apply policy

--- 4_create_table.sql
+++ 4_create_table.sql
@@ -1,3 +1,4 @@
+-- Creates all tables.
 CREATE TABLE users (
   sid         VARCHAR(8),
   name        VARCHAR(80) NOT NULL,
@@ -9,21 +10,21 @@ CREATE TABLE users (
 );
 
 CREATE TABLE addresses (
-  sid     VARCHAR(8)   NOT NULL,
-  type    VARCHAR(10)  NOT NULL,
-  address VARCHAR(100) NOT NULL,
+  sid         VARCHAR(8)   NOT NULL,
+  type        VARCHAR(10)  NOT NULL,
+  address     VARCHAR(100) NOT NULL,
   PRIMARY KEY (sid, type),
   FOREIGN KEY (sid) REFERENCES users(sid),
   CHECK       (type IN ('mailing', 'billing'))
 );
 
 CREATE TABLE fees (
-  id       NUMBER      GENERATED AS IDENTITY,
-  sid      VARCHAR(8)  NOT NULL,
-  type     VARCHAR(20) NOT NULL,
-  year     VARCHAR(5)  NOT NULL,
-  semester NUMBER(1)   NOT NULL,
-  amount   NUMBER(8,2) NOT NULL,
+  id          NUMBER      GENERATED AS IDENTITY,
+  sid         VARCHAR(8)  NOT NULL,
+  type        VARCHAR(20) NOT NULL,
+  year        VARCHAR(5)  NOT NULL,
+  semester    NUMBER(1)   NOT NULL,
+  amount      NUMBER(8,2) NOT NULL,
   PRIMARY KEY (id),
   FOREIGN KEY (sid)     REFERENCES users(sid),
   CHECK       (type     IN ('admin', 'tuition', 'misc')),
@@ -31,16 +32,16 @@ CREATE TABLE fees (
 );
 
 CREATE TABLE modules (
-  id       NUMBER          GENERATED AS IDENTITY,
-  code     VARCHAR(5)      NOT NULL,
-  title    VARCHAR(100)    NOT NULL,
-  year     VARCHAR(5)      NOT NULL,
-  semester NUMBER(1)       NOT NULL,
-  lecturer_sid VARCHAR(8)  NOT NULL,
-  PRIMARY KEY (id),
-  UNIQUE      (code, year, semester),
-  FOREIGN KEY (lecturer_sid) REFERENCES users(sid),
-  CHECK       (semester      IN (1, 2))
+  id           NUMBER       GENERATED AS IDENTITY,
+  code         VARCHAR(5)   NOT NULL,
+  title        VARCHAR(100) NOT NULL,
+  year         VARCHAR(5)   NOT NULL,
+  semester     NUMBER(1)    NOT NULL,
+  lecturer_sid VARCHAR(8)   NOT NULL,
+  PRIMARY KEY  (id),
+  UNIQUE       (code, year, semester),
+  FOREIGN KEY  (lecturer_sid) REFERENCES users(sid),
+  CHECK        (semester      IN (1, 2))
 );
 
 CREATE TABLE grades (
@@ -48,9 +49,47 @@ CREATE TABLE grades (
   student_sid  VARCHAR(8) NOT NULL,
   module_id    NUMBER     NOT NULL,
   letter_grade VARCHAR(1) NOT NULL,
-  PRIMARY KEY (id),
-  UNIQUE      (student_sid, module_id),
-  FOREIGN KEY (student_sid) REFERENCES users(sid),
-  FOREIGN KEY (module_id)   REFERENCES modules(id),
-  CHECK       (letter_grade IN ('A', 'B', 'C', 'D', 'F'))
+  PRIMARY KEY  (id),
+  UNIQUE       (student_sid, module_id),
+  FOREIGN KEY  (student_sid) REFERENCES users(sid),
+  FOREIGN KEY  (module_id)   REFERENCES modules(id),
+  CHECK        (letter_grade IN ('A', 'B', 'C', 'D', 'F'))
 );
+
+-- Assigns OLS policies.
+BEGIN
+    SA_POLICY_ADMIN.APPLY_TABLE_POLICY(
+        policy_name    => 'edu_ols',
+        schema_name    => 'lbacsys',
+        table_name     => 'users',
+        table_options  => 'all_control',
+        label_function => 'label_users()');
+
+    SA_POLICY_ADMIN.APPLY_TABLE_POLICY(
+        policy_name    => 'edu_ols',
+        schema_name    => 'lbacsys',
+        table_name     => 'addresses',
+        table_options  => 'all_control',
+        label_function => 'label_addresses(:new.type)');
+
+    SA_POLICY_ADMIN.APPLY_TABLE_POLICY(
+        policy_name    => 'edu_ols',
+        schema_name    => 'lbacsys',
+        table_name     => 'fees',
+        table_options  => 'all_control',
+        label_function => 'label_fees()');
+
+    SA_POLICY_ADMIN.APPLY_TABLE_POLICY(
+        policy_name    => 'edu_ols',
+        schema_name    => 'lbacsys',
+        table_name     => 'modules',
+        table_options  => 'all_control',
+        label_function => 'label_modules()');
+
+    SA_POLICY_ADMIN.APPLY_TABLE_POLICY(
+        policy_name    => 'edu_ols',
+        schema_name    => 'lbacsys',
+        table_name     => 'grades',
+        table_options  => 'all_control',
+        label_function => 'label_grades(:new.module_id)');
+END;
