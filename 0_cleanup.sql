111d06ced36fc23fe5e2cdaa627bec9c32c9051f
Yunpeng Niu <neilniuyunpeng@gmail.com>
2020-10-24 11:09:02 +0800

    Add setup code

---
+++ 0_cleanup.sql
@@ -0,0 +1,8 @@
+-- Checks whether Oracle OLS has been registered.
+SELECT STATUS FROM DBA_OLS_STATUS WHERE NAME = 'OLS_CONFIGURE_STATUS';
+
+-- Checks whether Oracle OLS has been enabled.
+SELECT VALUE FROM V$OPTION WHERE PARAMETER = 'Oracle Label Security';
+
+-- Unlocks OLS default administer.
+ALTER USER LBACSYS ACCOUNT UNLOCK IDENTIFIED BY "ols_admin";
