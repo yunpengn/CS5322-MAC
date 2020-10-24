4703d3acbda08e643a16e3bbd59258af27a9dcf1
Yunpeng Niu <neilniuyunpeng@gmail.com>
2020-10-24 23:35:00 +0800

    Fix

---
+++ 3_create_user.sql
@@ -0,0 +1,7 @@
+-- Add actual user accounts.
+CREATE USER hugh   IDENTIFIED BY hugh;
+CREATE USER damith IDENTIFIED BY damith;
+CREATE USER chris  IDENTIFIED BY chris;
+CREATE USER david  IDENTIFIED BY david;
+CREATE USER steve  IDENTIFIED BY steve;
+CREATE USER reza   IDENTIFIED BY reza;
