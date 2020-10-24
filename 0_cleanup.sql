52e43dfbc8593feb43d47dac193748a51ee6eaa0
Yunpeng Niu <neilniuyunpeng@gmail.com>
2020-10-24 17:35:46 +0800

    Fix

--- 0_cleanup.sql
+++ 0_cleanup.sql
@@ -6,9 +6,20 @@ SELECT VALUE FROM V$OPTION WHERE PARAMETER = 'Oracle Label Security';
 
 -- Unlocks OLS default administer.
 ALTER USER LBACSYS ACCOUNT UNLOCK IDENTIFIED BY "ols_admin";
-
--- Grants unlimited table space.
+GRANT ALL PRIVILEGES TO LBACSYS;
 ALTER USER LBACSYS QUOTA UNLIMITED ON USERS;
 
--- Grants necessary privileges.
-GRANT ALL PRIVILEGES TO LBACSYS;
+-- Kills all active connections.
+BEGIN
+  FOR r IN (select sid, serial# from v$session where username = 'EDU_ADMIN') LOOP
+    EXECUTE IMMEDIATE 'alter system kill session ''' || r.sid  || ','  || r.serial# || ''' immediate';
+  END LOOP;
+END;
+
+-- Drops existing user & records.
+DROP USER edu_admin CASCADE;
+
+-- Creates system user.
+CREATE USER edu_admin IDENTIFIED BY edu_admin;
+GRANT ALL PRIVILEGES TO edu_admin;
+ALTER USER edu_admin QUOTA UNLIMITED ON USERS;
